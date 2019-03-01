bfa852ba9aa02d3f99235b19d40eaac0e6b496e6
Mark Guldhammer <markguldhammer@gmail.com>
2019-03-01 16:15:57 +0100

    added images to stage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
-let type = "WebGL"
-  if(!PIXI.utils.isWebGLSupported()){
-    type = "canvas"
-  }
+PIXI.utils.sayHello();
 
-PIXI.utils.sayHello(type)
+let app = new PIXI.Application({
+  width: 800,
+  height: 550,
+  antialias: true,
+  transparent: true,
+  resolution: 1
+});
+
+document.body.appendChild(app.view);
+
+PIXI.loader
+  .add("background", "images/background.png")
+  .add("guy", "images/guy.png")
+  .load(setup);
+  
+let backgroundSprite, guySpirte;
+
+function setup() {
+  backgroundSprite = new PIXI.Sprite(PIXI.loader.resources["background"].texture);
+  guySprite = new PIXI.Sprite(PIXI.loader.resources["guy"].texture);
+  
+  app.stage.addChild(backgroundSprite);
+  app.stage.addChild(guySprite);
+}
