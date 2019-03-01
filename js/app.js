202f4a96531a9b908d3fbaa41e3724966ecf3f74
Mark Guldhammer <markguldhammer@gmail.com>
2019-03-01 19:24:04 +0100

    added initial tween animation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-PIXI.utils.sayHello();
-
 let app = new PIXI.Application({
   width: 800,
   height: 550,
@@ -15,12 +13,54 @@ PIXI.loader
   .add("guy", "images/guy.png")
   .load(setup);
   
-let backgroundSprite, guySpirte;
+let backgroundSprite, guySprite;
 
 function setup() {
   backgroundSprite = new PIXI.Sprite(PIXI.loader.resources["background"].texture);
+
   guySprite = new PIXI.Sprite(PIXI.loader.resources["guy"].texture);
+  guySprite.anchor.set(0.5, 0.5);
+
+  var path = new PIXI.tween.TweenPath();
+  path.moveTo(110, 100);
+  path.lineTo(715, 100);
+  path.lineTo(715, 470);
+  path.lineTo(155, 470);
+  path.lineTo(155, 140);
+  path.lineTo(675, 140);
+  path.lineTo(675, 430);
+  path.lineTo(190, 430);
+  path.lineTo(190, 175);
+  path.lineTo(630, 175);
+  path.lineTo(630, 390);
+  path.lineTo(230, 390);
+  path.lineTo(230, 210);
+  path.lineTo(595, 210);
+  path.lineTo(595, 360);
+  path.lineTo(270, 360);
+  path.lineTo(270, 245);
+  path.lineTo(560, 245);
+  path.lineTo(560, 330);
+  path.lineTo(320, 330);
+
+  var gPath = new PIXI.Graphics();
+  gPath.lineStyle(5, 0x00FF00, 1);
+  gPath.drawPath(path);
+
+  var tween = PIXI.tweenManager.createTween(guySprite);
+  tween.path = path;
+  tween.time = 10000;
+  tween.loop = false;
+  tween.start();
   
   app.stage.addChild(backgroundSprite);
   app.stage.addChild(guySprite);
+  app.stage.addChild(gPath);
+  
+  app.ticker.add(delta => animationLoop(delta));
 }
+
+function animationLoop(delta) {
+  PIXI.tweenManager.update();
+}
+
