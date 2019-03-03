c407b4363aefb3574e594ee111a59ab5c90a3f20
Mark Guldhammer <markguldhammer@gmail.com>
2019-03-03 12:26:52 +0100

    added initial start button functionality

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ PIXI.loader
   .add("button", "images/button.png")
   .load(setup);
   
-let backgroundSprite, guySprite, buttonSprite;
+let backgroundSprite, guySprite, buttonSprite, isReversed = false;
 
 function setup() {
   backgroundSprite = new PIXI.Sprite(PIXI.loader.resources["background"].texture);
@@ -24,6 +24,27 @@ function setup() {
   buttonSprite.scale.set(0.2, 0.2);
   buttonSprite.x = app.renderer.width / 2;
   buttonSprite.y = app.renderer.height;
+  buttonSprite.interactive = true;
+  buttonSprite.cursor = "pointer";
+  buttonSprite.click = function() {
+    if (tween.active) {
+      isReversed = !isReversed;
+      setGuySpriteOrientation(isReversed);
+      
+      tween.reset();
+      tween.pathReverse = isReversed;
+      tween.start();
+    } else if (tween.isEnded) {
+      isReversed = !isReversed;
+      setGuySpriteOrientation(isReversed);
+
+      tween.reset();
+      tween.pathReverse = isReversed;
+      tween.start();
+    } else {
+      tween.start();
+    }
+  };
 
   guySprite = new PIXI.Sprite(PIXI.loader.resources["guy"].texture);
   guySprite.anchor.set(0.5, 0.5);
@@ -56,19 +77,27 @@ function setup() {
 
   var tween = PIXI.tweenManager.createTween(guySprite);
   tween.path = path;
-  tween.time = 10000;
+  tween.time = 5000;
   tween.loop = false;
-  tween.start();
+  //tween.pathReverse = true;
+  //tween.start();
   
   app.stage.addChild(backgroundSprite);
   app.stage.addChild(guySprite);
   app.stage.addChild(gPath);
   app.stage.addChild(buttonSprite);
   
-  app.ticker.add(delta => animationLoop(delta));
+  app.ticker.add(delta => animationLoop());
 }
 
-function animationLoop(delta) {
+function animationLoop() {
   PIXI.tweenManager.update();
 }
 
+function setGuySpriteOrientation(isReversed) {
+  if (isReversed) {
+    guySprite.scale.x = -1;
+  } else {
+    guySprite.scale.x = 1;
+  }
+}
